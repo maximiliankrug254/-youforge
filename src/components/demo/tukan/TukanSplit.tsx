2deddf09e701a1dab92e1ded56b75f7ec5e7b64c
Max <hallo@you-forge.de>
2026-09-07 09:52:30 +0800

    Drop unused TUKAN one-pager files so production typecheck can pass.

--- src/components/demo/tukan/TukanSplit.tsx
+++
@@ -1,8 +0,0 @@
-export function TukanSplit({ children }: { children: string }) {
-  return (
-    <span className="tukan-split">
-      <span>{children}</span>
-      <span aria-hidden>{children}</span>
-    </span>
-  );
-}
